f87fef84368fbb2f1dd1dddf142850b3713f3be6
Derek Liu <liu.derekt@gmail.com>
2016-11-15 12:04:15 -0800

    read subreddit entries

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      default: []
+    };
+
+    this.getSubreddit();
+  }
+
+  getSubreddit() {
+    console.log('inside getSubreddit');
+    fetch(`http://www.reddit.com/r/aww/hot.json`)
+      .then(res => res.json())
+      .then(body => console.log('body', body.data.children))
+      .catch(err => console.log('error', err));
+  }
+
   render() {
     return (
       <div className='App'>
